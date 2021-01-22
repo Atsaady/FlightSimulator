2f4cefcdb09022efbd9766001d41da0ce8e08e4a
Atsaady <54797664+Atsaady@users.noreply.github.com>
2021-01-22 12:58:18 +0200

    Changed the jar files to a new ones.

--- runClient.bat
+++ runClient.bat
@@ -1,1 +1,1 @@
-java --module-path C:\Users\atsaa\Downloads\javafx-sdk-11.0.2\lib --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar PTM_Project1.jar
+java --module-path C:\Users\atsaa\Downloads\javafx-sdk-11.0.2\lib\ --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar ""Client.jar""
--- runServer.bat
+++ runServer.bat
@@ -1,1 +1,1 @@
-java --module-path C:\Users\atsaa\Downloads\javafx-sdk-11.0.2\lib\ --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar PTM_Project2.jar
+java --module-path C:\Users\atsaa\Downloads\javafx-sdk-11.0.2\lib\ --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar Server.jar
